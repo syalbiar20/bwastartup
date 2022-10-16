83d814e564da54ef3e896c7a23689cfce7f196cf
syalbiar20 <syalbi.ar20@gmail.com>
2022-10-16 10:40:59 +0700

    Get campaign Transactions

--- handler/transaction.go
+++ handler/transaction.go
@@ -1,1 +1,49 @@
 package handler
+
+import (
+	"bwastartup/helper"
+	"bwastartup/transaction"
+	"bwastartup/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// parameter di uri
+// tangkap parameter mapping input struct
+// panggil service, input struct sebagai parameter
+// service, berbekal campaign id bisa panggil repo
+// repo mencari data transaction suatu campaign
+
+type transactionHandler struct {
+	service transaction.Service
+}
+
+func NewTransactionHandler(service transaction.Service) *transactionHandler {
+	return &transactionHandler{service}
+}
+
+func (h *transactionHandler) GetCampaignTransactions(c *gin.Context) {
+	var input transaction.GetCampaignTransactionsInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign's transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	currentUser := c.MustGet("currentUser").(user.User)
+
+	input.User = currentUser
+
+	transactions, err := h.service.GetTransactionsByCampaignID(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign's transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Campaign's transactions", http.StatusOK, "success", transaction.FormatCampaignTransactions(transactions))
+	c.JSON(http.StatusOK, response)
+}
